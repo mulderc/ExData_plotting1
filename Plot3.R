a2b13fccf98eeb83454861f969f14cf8f78544ae
mulderc <cameron.mulder@gmail.com>
2018-03-19 00:25:15 -0700

    more testing on cex value

--- Plot3.R
+++ Plot3.R
@@ -19,7 +19,7 @@ par(mfrow = c(1,1))
 plot(HH_Power_ConsumptionFILTERED$DateTime,HH_Power_ConsumptionFILTERED$Sub_metering_1, type = "l", col = "green", xlab = "", ylab = "Energy sub metering")
 lines(HH_Power_ConsumptionFILTERED$DateTime,HH_Power_ConsumptionFILTERED$Sub_metering_2, type = "l", col = "red")
 lines(HH_Power_ConsumptionFILTERED$DateTime,HH_Power_ConsumptionFILTERED$Sub_metering_3, type = "l", col = "blue")
-legend("topright", legend = c("Sub_mettering_1","Sub_mettering_2", "Sub_mettering_3"), lty=c(1,1), lwd=c(2,2,2),col=c("green", "red", "blue"), cex = .9, inset = 0)
+legend("topright", legend = c("Sub_mettering_1","Sub_mettering_2", "Sub_mettering_3"), lty=c(1,1), lwd=c(2,2,2),col=c("green", "red", "blue"), cex = 1, inset = 0)
 #dev.print(png, "plot3.png", width = 480, height = 480)
 dev.off()
 
